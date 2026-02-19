e20c49e1a248748d4ec5b2328693318bb1251797
Jagadeesh-K3607 <jagadeesh.k2k1@gmai.com>
2026-02-19 16:33:06 +0530

    Conversation changes2

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+    PERSON1=Ramya
+    PERSON2=Jagadeesh
+echo "$PERSON1:: Hello $PERSON2 How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. how are u doing?"
+echo "$PERSON1:: Doing fine. what are you learing $PERSON2"
+echo "$PERSON2:: I am learning shell. what about you"
+echo "$PERSON1:: I am learning Terraform"
