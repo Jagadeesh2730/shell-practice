7a5076fa449ce6ae31e5b92283248845e0e6bf3f
Jagadeesh-K3607 <jagadeesh.k2k1@gmai.com>
2026-02-19 16:40:25 +0530

    Conversation changes3

--- 02-conversion.sh
+++ 02-conversion.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-    PERSON1=Ramya
-    PERSON2=Jagadeesh
-echo "$PERSON1:: Hello $PERSON2 How are you?"
-echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. how are u doing?"
-echo "$PERSON1:: Doing fine. what are you learing $PERSON2"
-echo "$PERSON2:: I am learning shell. what about you"
-echo "$PERSON1:: I am learning Terraform"
+    
+echo "jagadeesh:: Hello rakesh How are you?"
+echo "rakesh:: Hi jagadeesh, I am fine thanks for asking. how are u doing?"
+echo "jagadeesh:: Doing fine. what are you learing rakesh"
+echo "rakesh:: I am learning shell. what about you"
+echo "jagadeesh:: I am learning Terraform"
---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+    PERSON1=$1
+    PERSON2=$2
+echo "$PERSON1:: Hello $PERSON2 How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. how are u doing?"
+echo "$PERSON1:: Doing fine. what are you learing $PERSON2"
+echo "$PERSON2:: I am learning shell. what about you"
+echo "$PERSON1:: I am learning Terraform"
