e4f61d0e87e8b7f5782e7387639e65010cde325a
Jagadeesh-K3607 <jagadeesh.k2k1@gmai.com>
2026-02-19 16:09:15 +0530

    Conversation

---
+++ 02-conversion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "jagadeesh:: Hello Rakesh How are you?"
+echo "Rakesh:: Hi Jagadeesh, I am fine thanks for asking. how are u doing?"
+echo "Jagadeesh:: Doing fine. what are you learing Rakesh"
+echo "Rakesh:: I am learning shell. what about you"
