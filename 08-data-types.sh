535a47b0ccfe81a7030fa1c22a3f2c01bbbba4bd
Jagadeesh-K3607 <jagadeesh.k2k1@gmai.com>
2026-02-20 10:22:56 +0530

    datatypes1arrays

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -6,3 +6,13 @@ NUM2=200
 SUM=$(($NUM1+$NUM2))
 
 echo "Sum is :: $SUM"
+
+#Arrays
+
+FRUITS=("Apple" "Banana" "Pomi")
+
+echo "Fruits are: $Fruits[@]"
+
+echo "Fruits are: $Fruits[1]"
+
+echo "Fruits are: $Fruits[2]"
